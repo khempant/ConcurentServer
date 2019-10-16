b359d39193a7ac286b2e348e4306cc14be837d93
Khem Pant <khem0026@edu.easj.dk>
2019-10-16 21:28:24 +0200

    ServerSet

--- Assignment4ConcurrentServer/Server.cs
+++ Assignment4ConcurrentServer/Server.cs
@@ -1,16 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Net;
+using System.IO;
 using System.Net.Sockets;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Assignment4ConcurrentServer
 {
-    class Server
+    class Program
     {
-        static List<Book> books = new List<Book>()
+        [Obsolete]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+            TcpListener serverSocket = new TcpListener(6789);
+            serverSocket.Start();
+
+            Console.WriteLine("server started witing for connection!");
+
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            //Socket connectionSocket = serverSocket.AcceptSocket();
+            Console.WriteLine("Server activated");
+
+            Stream ns = connectionSocket.GetStream();
+            // Stream ns = new NetworkStream(connectionSocket);
+
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true; // enable automatic flushing
+
+            string message = sr.ReadLine();
+            string answer = "";
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+
+            ns.Close();
+            connectionSocket.Close();
+            serverSocket.Stop();
+        }
+        public List<Book> books = new List<Book>()
         {
             new Book("FreedomFighter","Popular",68,"1234567896543"),
             new Book("CountryLove","Khem",123,"1239567896542"),
@@ -19,52 +53,10 @@ namespace Assignment4ConcurrentServer
             new Book("Algebra","Michel",698,"1234568896513"),
             new Book("Lyrics","William",65,"1234567896842"),
         };
-        public void Start()
-        {
-            TcpListener server = null;
-            try
-            {
-                // Set the TcpListener
-                Int32 port = 4646;
-                IPAddress localAddr = IPAddress.Loopback;
-
-                int clientNumber = 0;
-
-                // TcpListener server = new TcpListener(port);
-                server = new TcpListener(localAddr, port);
-
-                // Start listening for client requests.
-                server.Start();
-
-                // Enter the listening loop.
-                while (true)
-                {
-                    Console.Write("Waiting for a connection... ");
-
-                    // Perform a blocking call to accept requests.
-                    // You could also user server.AcceptSocket() here.
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-
-                    Task.Run(() => HandleStream(client, ref clientNumber));
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-            }
-            finally
-            {
-                // Stop listening for new clients.
-                server.Stop();
-            }
-            Console.WriteLine("\nHit enter to continue...");
-            Console.Read();
-        }
-
-        private void HandleStream(TcpClient client, ref int clientNumber)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
+
+        
+    
+    
+
